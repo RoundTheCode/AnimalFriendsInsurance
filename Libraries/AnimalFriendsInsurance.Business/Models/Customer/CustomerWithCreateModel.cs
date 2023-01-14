9c2fce2083f01d11969868060e597bf4afc5f599
David Grace <davidgrace00@gmail.com>
2023-01-14 13:43:25 +0000

    Add customer create model

---
+++ Libraries/AnimalFriendsInsurance.Business/Models/Customer/CustomerWithCreateModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFriendsInsurance.Business.Models.Customer
+{
+    /// <summary>
+    /// API request containing customer data
+    /// </summary>
+    public class CustomerWithCreateModel
+    {
+        /// <summary>
+        /// Customer first name
+        /// </summary>
+        [MinLength(3, ErrorMessage = "First name must be at least 3 characters"), 
+         MaxLength(50, ErrorMessage ="First name must be no more than 50 characters")]
+        public string? FirstName { get; init; }
+
+        /// <summary>
+        /// Customer surname
+        /// </summary>
+        [MinLength(3, ErrorMessage = "Surname must be at least 3 characters"),
+        MaxLength(50, ErrorMessage = "Surname must be no more than 50 characters")]
+        public string? Surname { get; init; }
+
+        /// <summary>
+        /// Policy reference number
+        /// </summary>
+        [RegularExpression(@"^([A-Z]{2})\-([0-9]{6})$")]
+        public string? PolicyReferenceNumber { get; init; }
+
+        /// <summary>
+        /// Customer's date of birth
+        /// </summary>
+        public DateTime? DateOfBirth { get; init; }
+
+        /// <summary>
+        /// Customer's email address
+        /// </summary>
+        [EmailAddress]
+        public string? Email { get; init; }
+
+    }
+}
